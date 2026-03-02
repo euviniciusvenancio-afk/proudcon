6aa9fc719fce2460392736ba09afc59e05f6bd74
Luiza Ferrari (teste 2) <luizaferrari879@gmail.com>
2026-03-02 02:40:07 +0000

    troque essa imagem para a imagem do link: https://instagram.fjdf4-1.fna.

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'instagram.fjdf4-1.fna.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
